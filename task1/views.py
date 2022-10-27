4ed941c70ef584a66f1f552248c1a78ed9d45816
udosaint <udosaintdanielokoye@gmail.com>
2022-10-27 17:05:26 +0100

    HNG-TASTK-1

---
+++ task1/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+# Create your views here.
+
+data = [
+    {'SlackUsername': "Udosaint", 'backend': True, 'age': 26, 'bio': 'Hi am a fullstack developer and also a Data Analyst, Happy to meet you all'}
+]
+
+
+def Index(request):
+    context = {'data': data}
+    return HttpResponse(data)
